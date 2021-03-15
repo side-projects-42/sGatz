7aaf6e967214a935837d6dc99284571681dd93f3
Sarah Rainsberger <5098874+sarah11918@users.noreply.github.com>
2021-03-14 21:24:58 -0300

    put query at end

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,19 +3,6 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 
-export const query = graphql`
-  query($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        date(formatString: "DD MMMM, YYYY")
-      }
-      timeToRead
-      html
-    }
-  }
-`
-
 const BlogPost = props => {
   return (
     <Layout>
@@ -33,5 +20,18 @@ const BlogPost = props => {
     </Layout>
   )
 }
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date(formatString: "DD MMMM, YYYY")
+      }
+      timeToRead
+      html
+    }
+  }
+`
 
 export default BlogPost
